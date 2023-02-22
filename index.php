179ce14892567acf912267f6efe58d5027c29754
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-22 15:00:12 +0100

    Add verifiche collegamenti file

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
+<?php
+    include __DIR__ .'/script.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- collegamento css -->
+        <link rel="stylesheet" href="css/style.css">
+
         <title>PHP ToDo List JSON</title>
     </head>
     <body>
         <div id="app">
+            <h1>{{ message }}</h1>
 
+            <p><?php echo $message ?></p>
         </div>
         
         <!-- Collegamento acios, vue e js -->
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp ({
     data () {
         return {
-
+            message: "vue e js ok"
         }
     }
 }).mount('#app');
